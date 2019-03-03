cb3b63949dfe29d45c539747999fa2e2ac635cfd
Ilya Kiyanov <ikiyanov@protonmail.com>
2019-03-03 16:01:48 +0300

    adds functional for js-script that creates sql with changes

--- ms-sql/edit-sql-script.js
+++ ms-sql/edit-sql-script.js
@@ -2,4 +2,9 @@ var fs = require('fs');
 
 var sqlScript = fs.readFileSync('ws-marathon-2016-db.sql', 'utf-8');
 
-console.log(sqlScript);
+var regex = sqlScript.replace(/DateTime/g, "DateTime2");
+
+fs.writeFile("updated-sqript.sql", regex, function(err, data) {
+    if (err) console.log(err);
+    console.log("Script successfully updated!")
+});
